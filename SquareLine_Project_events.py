f33119222428d475decf347c298210e7e5e52347
Fabio Zigliani <fabio.zigliani@gmail.com>
2024-09-09 19:15:35 +1200

    Initial project with Error in Include Path

---
+++ SquareLine_Project_events.py
@@ -0,0 +1,20 @@
+
+def fnbtnSinlgeClick(event_struct):
+    return
+
+
+def fnbtnSingleClick(event_struct):
+    return
+
+
+def fnbtnSingleLongPress(event_struct):
+    return
+
+
+def OnRotateClicked(event_struct):
+    return
+
+
+def OnAddOneClicked(event_struct):
+    return
+
